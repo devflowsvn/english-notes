fa9736ab2fa84739af7fa300e9caef1bc193f249
Devflows <devflows.programming@gmail.com>
2025-11-12 22:00:29 +0700

    dl4

--- app/data/get-questions.ts
+++ app/data/get-questions.ts
@@ -1,23 +1,3 @@
 import { prisma } from "@/lib/db";
 
-export async function getQuestionsOver12Hours() {
-  // Tính mốc thời gian 12 tiếng trước hiện tại
-  const twelveHoursAgo = new Date(Date.now() - 12 * 60 * 60 * 1000);
-
-  // Lấy những câu hỏi mà learnedAt < twelveHoursAgo
-  const questions = await prisma.question.findMany({
-    where: {
-      learnedAt: {
-        lt: twelveHoursAgo,
-      },
-    },
-    select: {
-      id: true,
-      question: true,
-      answer: true,
-      learnedAt: true,
-    },
-  });
-
-  return questions;
-}
+export async function getQuestionsOver12Hours() {}
